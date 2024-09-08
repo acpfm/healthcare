aff85f30f3204fad648a152dc333f8dcdfebe2b2
acpfm <ana.pasti.ferreira@gmail.com>
2024-09-08 14:31:08 +0100

    Correções para main executar sem erros.

--- src/main/java/com/acpfm/healthcare/controller/UserController.java
+++ src/main/java/com/acpfm/healthcare/controller/UserController.java
@@ -3,15 +3,15 @@ package com.acpfm.healthcare.controller;
 import com.acpfm.healthcare.model.User;
 import com.acpfm.healthcare.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
@@ -22,14 +22,14 @@ public class UserController {
         return "users";
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public String registerUser(@ModelAttribute User user, Model model){
         userService.registerUser(user);
         model.addAttribute("users",userService.getAllUsers());
         return "fragments/user-list :: userList";
     }
 
-    @PostMapping
+    @PostMapping("/search")
     public String searchUsers(@RequestParam String username, Model model){
         List<User> users = userService.searchUsers(username);
         model.addAttribute("users",users);
